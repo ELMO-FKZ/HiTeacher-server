704a54cb735a332af51f1d62b0e949f986b352a4
EL MOKHTAR <122396078+ELMO-FKZ@users.noreply.github.com>
2023-09-29 23:37:46 +0200

    Fixing deployment problems.

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use(express.urlencoded( {extended: true} ));
 
 // middleware
 app.use(express.json());
+// app.use(cors({origin: 'http://localhost:5173', credentials: true}));
 app.use(cors({origin: 'https://classroom-kolz.onrender.com', credentials: true}));
 
 // Copression
